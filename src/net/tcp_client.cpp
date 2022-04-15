41221aa68c54c5a399c404770105870f92c0679d
sh1799 <sh1799@closeli.cn>
2022-04-16 03:01:17 +0800

    修改tcp_client的start_connect逻辑

--- src/net/tcp_client.cpp
+++ src/net/tcp_client.cpp
@@ -25,12 +25,12 @@
 #include "tcp_client.hpp"
 #include "Util/onceToken.h"
 #include "socket_base.hpp"
-using super_type = typename tcp_client::super_type;
 
 #ifdef SSL_ENABLE
-tcp_client::tcp_client(bool current_thread, const std::shared_ptr<context> &_context_) : tcp_session(socket_helper::create_bind_socket(current_thread), _context_) {}
+tcp_client::tcp_client(bool current_thread, const std::shared_ptr<context> &_context_)
+    : tcp_session(socket_helper::create_tcp_socket(current_thread), _context_) {}
 #else
-tcp_client::tcp_client(bool current_thread) : tcp_session(socket_helper::create_bind_socket(current_thread)) {}
+tcp_client::tcp_client(bool current_thread) : tcp_session(socket_helper::create_tcp_socket(current_thread)) {}
 #endif
 
 void tcp_client::on_connected() {
@@ -51,12 +51,13 @@ void tcp_client::bind_local(const std::string &ip, unsigned short port) {
 
 void tcp_client::start_connect(const std::string &ip, unsigned short port) {
     toolkit::onceToken token([&]() { mtx.lock(); }, [&]() { mtx.unlock(); });
-    close();
-    open_l(this->local_point.address().is_v4());
-    super_type::bind(this->local_point);
+    auto &sock = get_sock();
+    sock.close();
+    sock.open(this->local_point.address().is_v4() ? asio::ip::tcp::v4() : asio::ip::tcp::v6());
+    sock.bind(this->local_point);
     endpoint end_point(asio::ip::make_address(ip), port);
     auto stronger_self(std::static_pointer_cast<tcp_client>(shared_from_this()));
-    super_type::async_connect(end_point, [stronger_self](const std::error_code &e) {
+    get_sock().async_connect(end_point, [stronger_self](const std::error_code &e) {
         if (e) {
             Error(e.message());
             stronger_self->onError(e);
@@ -66,10 +67,10 @@ void tcp_client::start_connect(const std::string &ip, unsigned short port) {
         return stronger_self->read_l();
     });
 }
-void tcp_client::send_loop(basic_buffer<char>& buf){
-    auto stronger_self(shared_from_this());
-    std::shared_ptr<basic_buffer<char>> buff = std::make_shared<basic_buffer<char>>(std::move(buf));
-    get_poller().async([stronger_self, buff](){
-        stronger_self->send(*buff);
-    });
+void tcp_client::send_loop(basic_buffer<char> &buf) {
+//    auto stronger_self(shared_from_this());
+//    std::shared_ptr<basic_buffer<char>> buff = std::make_shared<basic_buffer<char>>(std::move(buf));
+//    get_poller().async([stronger_self, buff]() {
+//        stronger_self->send(*buff);
+//    });
 }
