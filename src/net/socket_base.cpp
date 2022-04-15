a3b12cd21fd187508fd4c5520ca0edc42ea160fe
sh1799 <sh1799@closeli.cn>
2022-04-16 02:59:53 +0800

    增加udp helper的创建

--- src/net/socket_base.cpp
+++ src/net/socket_base.cpp
@@ -3,7 +3,12 @@
 //
 #include "socket_base.hpp"
 #include "event_poller_pool.hpp"
-std::pair<event_poller::Ptr, std::shared_ptr<asio::ip::tcp::socket>> socket_helper::create_bind_socket(bool current_thread){
+std::pair<event_poller::Ptr, std::shared_ptr<asio::ip::tcp::socket>> socket_helper::create_tcp_socket(bool current_thread){
     auto poller = event_poller_pool::Instance().get_poller(current_thread);
     return std::make_pair(poller, std::make_shared<asio::ip::tcp::socket>(poller->get_executor()));
+}
+
+std::pair<event_poller::Ptr, std::shared_ptr<asio::ip::udp::socket>> socket_helper::create_udp_socket(bool current_thread){
+    auto poller = event_poller_pool::Instance().get_poller(current_thread);
+    return std::make_pair(poller, std::make_shared<asio::ip::udp::socket>(poller->get_executor()));
 }
