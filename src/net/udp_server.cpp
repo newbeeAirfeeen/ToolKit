0ac08823ddfef26fd0e95e2e142b4f6e07a2d499
sh1799 <sh1799@closeli.cn>
2022-04-16 03:02:43 +0800

    增加udp_server实现

--- src/net/udp_server.cpp
+++ src/net/udp_server.cpp
@@ -23,3 +23,62 @@
 * SOFTWARE.
 */
 #include "udp_server.hpp"
+#include "event_poller_pool.hpp"
+#include "spdlog/logger.hpp"
+#include "udp_session.hpp"
+udp_server::udp_server(event_poller &poller) : poller(poller), asio::ip::udp::socket(poller.get_executor()) {
+    buff.resize(2048);
+}
+
+void udp_server::stop() {
+}
+
+void udp_server::read_datagram() {
+    auto stronger_self(shared_from_this());
+    std::shared_ptr<endpoint_type> endpoint = std::make_shared<endpoint_type>();
+    auto recv_func = [stronger_self, endpoint](const std::error_code &er, size_t length) {
+        if (er) {
+            Error(er.message());
+        }
+        if (length > 0) {
+            stronger_self->buff.resize(length);
+            stronger_self->transmit_message(stronger_self->buff, *endpoint);
+            stronger_self->buff.resize(2048);
+        }
+        return stronger_self->read_datagram();
+    };
+    async_receive_from(asio::buffer(const_cast<char *>(stronger_self->buff.data()), stronger_self->buff.size()), *endpoint, recv_func);
+}
+
+void udp_server::transmit_message(basic_buffer<char> &buf, const endpoint_type &endpoint) {
+    auto it = session_map.find(endpoint);
+    if (it == session_map.end()) {
+        auto session = std::make_shared<udp_session>(*event_poller_pool::Instance().get_poller(false), _context, static_cast<asio::ip::udp::socket&>(*this));
+        session_map.emplace(endpoint, session);
+        //开始会话
+        session->attach_server(this);
+        session->begin_session();
+        session->launchRecv(buf, endpoint);
+        return;
+    }
+    //刷新定时器
+    it->second->flush();
+    it->second->launchRecv(buf, endpoint);
+}
+
+void udp_server::launchError(const endpoint_type &endpoint) {
+    //切换到自己的线程
+    std::weak_ptr<udp_server> self(std::static_pointer_cast<udp_server>(shared_from_this()));
+    poller.async([endpoint, self]() {
+        auto stronger_self = self.lock();
+        if (!stronger_self) {
+            return;
+        }
+        stronger_self->remove_session(endpoint);
+    });
+}
+
+void udp_server::remove_session(const endpoint_type &endpoint) {
+    auto begin = session_map.begin();
+    session_map.erase(endpoint);
+}
--- src/net/udp_server.hpp
+++ src/net/udp_server.hpp
@@ -24,14 +24,55 @@
 */
 #ifndef TOOLKIT_UDP_SERVER_HPP
 #define TOOLKIT_UDP_SERVER_HPP
-#include <Util/nocopyable.hpp>
+#include <asio.hpp>
 #include <memory>
-
-class udp_server : public std::enable_shared_from_this<udp_server>, public noncopyable{
+#include <unordered_map>
+#include <mutex>
+#include "buffer.hpp"
+#include "event_poller.hpp"
+#include "net/ssl/context.hpp"
+class udp_session;
+class udp_server : public std::enable_shared_from_this<udp_server>, public asio::ip::udp::socket {
+    friend class udp_session;
 public:
-
-
-
+    using pointer = std::shared_ptr<udp_server>;
+    using endpoint_type = typename asio::ip::udp::socket::endpoint_type;
+    using super_type = asio::ip::udp::socket;
+public:
+    explicit udp_server(event_poller& poller);
+    /**
+     * 开始一个udp套接字监听
+     * @tparam session_type 继承于udp_session的session_type
+     * @param port 端口
+     * @param ip ipv4或者ipv6地址
+     */
+#ifdef SSL_ENABLE
+    template<typename session_type>
+    void start(unsigned short port, const std::string& ip = "0.0.0.0", const std::shared_ptr<context>& _context = nullptr){
+        this->_context = _context;
+#else
+    template<typename session_type>
+    void start(unsigned short port, const std::string& ip = "0.0.0.0"){
+#endif
+        endpoint_type endpoint(asio::ip::make_address(ip), port);
+        super_type::open(endpoint.address().is_v4() ? asio::ip::udp::v4() : asio::ip::udp::v6());
+        super_type::bind(endpoint);
+        Info("udp server on {}:{}", ip, port);
+        return read_datagram();
+    }
+    void stop();
+private:
+    void read_datagram();
+    void transmit_message(basic_buffer<char>& buf, const endpoint_type& endpoint);
+    void launchError(const endpoint_type& endpoint);
+    void remove_session(const endpoint_type& endpoint);
+private:
+    event_poller& poller;
+    basic_buffer<char> buff;
+    std::unordered_map<endpoint_type, std::shared_ptr<udp_session>> session_map;
+#ifdef SSL_ENABLE
+    std::shared_ptr<context> _context;
+#endif
 };
 
 
