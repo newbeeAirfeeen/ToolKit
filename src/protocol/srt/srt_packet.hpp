345dc58427fcfaafe8ede57ef7735a90089529b3
newbeeAirfeeen <937176211@qq.com>
2022-04-21 21:20:33 +0800

    增加srt的packet说明

---
+++ src/protocol/srt/srt_packet.hpp
@@ -0,0 +1,50 @@
+﻿/*
+* @file_name: srt_packet.hpp
+* @date: 2022/04/21
+* @author: oaho
+* Copyright @ hz oaho, All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#ifndef TOOLKIT_SRT_PACKET_HPP
+#define TOOLKIT_SRT_PACKET_HPP
+
+/**
+ * The SRT Protocol draft-sharabayko-srt-01
+ *
+ * Page 6
+ * SRT Packet are transmitted as UDP payload [RFC0768], Every UDP packet carrying SRT traffic
+ * contains an SRT header immediately after the UDP header
+ *
+ *                0                   1                   2                   3
+ *                0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+ *               +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *               |             SrcPort           |             DstPort           |
+ *               +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *               |             Len               |             ChkSum            |
+ *               +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *               |                                                               |
+ *               +                            SRT Packet                         +
+ *               |                                                               |
+ *               +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *                              Figure 1: SRT packet as UDP payload
+ */
+
+
+#endif//TOOLKIT_SRT_PACKET_HPP
