e3148cada672b28fdacda9753765ab69d6b3efbe
sh1799 <sh1799@closeli.cn>
2022-04-28 11:16:35 +0800

    增加srt包定义

---
+++ src/protocol/srt/srt_packet.cpp
@@ -0,0 +1,46 @@
+﻿/*
+* @file_name: srt_packet.cpp
+* @date: 2022/04/27
+* @author: oaho
+* Copyright @ hz oaho, All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#include "srt_packet.hpp"
+namespace srt{
+    uint32_t srt_packet::get_packet_header() const{
+        return 0;
+    }
+
+    uint32_t srt_packet::get_sequence() const {
+        return 0;
+    }
+
+    const char* srt_packet::data() const {
+        return nullptr;
+    }
+
+    size_t srt_packet::size() const {
+        return 0;
+    }
+
+    control_type srt_packet_helper::get_control_packet_type(uint32_t header){
+        return control_type::ack;
+    }
+};
