fff1816b5fc3561ac92fc5c780340e94fc0f23a0
sh1799 <sh1799@closeli.cn>
2022-04-29 13:13:36 +0800

    增加control_packet_context的定义

--- src/protocol/srt/srt_packet_profile.cpp
+++ src/protocol/srt/srt_packet_profile.cpp
@@ -23,10 +23,130 @@
 * SOFTWARE.
 */
 #include "srt_packet_profile.hpp"
+#include "srt_packet.hpp"
+#include "Util/endian.hpp"
+#include "err.hpp"
+#include <type_traits>
+#include <new>
 namespace srt{
 
-    bool control_packet::value(uint32_t val){
-        return false;
+    control_packet_context::control_packet_context(const srt_packet& pkt){
+        const char* data = pkt.data();
+        type = static_cast<control_type>(load_be16(data) & 0x7FFF);
+        sub_type = load_be16(&(data[2]));
+        type_information = load_be32(&(data[4]));
+        time_stamp = load_be32(&(data[8]));
+        socket_id = load_be32(&(data[12]));
+        raw_data = pkt.data() + 16;
+        raw_size = pkt.size() - 16;
+    }
+
+    control_type control_packet_context::get_control_type() const{
+        return type;
+    }
+
+    unsigned short control_packet_context::get_sub_type() const{
+        return sub_type;
+    }
+
+    uint32_t control_packet_context::get_type_information() const{
+        return type_information;
+    }
+
+    uint32_t control_packet_context::get_timestamp() const{
+        return time_stamp;
+    }
+
+    uint32_t control_packet_context::get_socket_id() const{
+        return socket_id;
+    }
+
+    const char* control_packet_context::data() const{
+        return raw_data;
+    }
+
+    size_t control_packet_context::size() const{
+        return raw_size;
+    }
+
+    data_packet_context::data_packet_context(const srt_packet& pkt){
+        const char* data = pkt.data();
+        packet_sequence_number = static_cast<control_type>(load_be32(data) & 0x7FFFFFFF);
+        packet_position = data[4] & 0xC0;
+        should_order_ = (data[4] & 0x20) == 1;
+        key_encrypt_ = data[4] & 0x18;
+        is_retransmitted_ = data[4] & 0x4;
+        message_number = load_be32(&(data[4])) & 0x3FFFFFF;
+        time_stamp = load_be32(&(data[8]));
+        socket_id = load_be32(&(data[12]));
+        raw_data = data + 16;
+        length = pkt.size() - 16;
+    }
+
+    uint32_t data_packet_context::get_sequence() const{
+        return packet_sequence_number;
+    }
+
+    uint32_t data_packet_context::get_message_sequence() const{
+        return message_number;
+    }
+
+    uint32_t data_packet_context::get_timestamp() const{
+        return time_stamp;
+    }
+
+    uint32_t data_packet_context::get_socket_id() const{
+        return socket_id;
+    }
+
+    bool data_packet_context::is_first_packet() const{
+        return packet_position == 0b10;
+    }
+
+    bool data_packet_context::is_single_packet() const{
+        return packet_position == 0b11;
+    }
+
+    bool data_packet_context::is_middle_packet() const{
+        return packet_position == 0;
+    }
+
+    bool data_packet_context::is_last_packet() const{
+        return packet_position == 1;
+    }
+
+    bool data_packet_context::should_order() const{
+        return should_order_;
+    }
+
+    int data_packet_context::key_encrypt() const{
+        return key_encrypt_;
+    }
+
+    bool data_packet_context::is_retransmitted() const{
+        return is_retransmitted_;
+    }
+
+    const char* data_packet_context::data() const{
+        return raw_data;
+    }
+
+    size_t data_packet_context::size() const{
+        return length;
+    }
+
+    packet_context::packet_context(const srt_packet& pkt){
+        char* data = const_cast<char*>(pkt.data());
+        data_ptr = new (data) data_packet_context(pkt);
+        control_ptr = new (data) control_packet_context(pkt);
+    }
+
+    const data_packet_context& packet_context::load_as_data() const {
+        return *data_ptr;
+    }
+
+    const control_packet_context& packet_context::load_as_control() const {
+        return *control_ptr;
     }
 
     bool ack::value(uint32_t val){
--- src/protocol/srt/srt_packet_profile.hpp
+++ src/protocol/srt/srt_packet_profile.hpp
@@ -24,7 +24,8 @@
 */
 #ifndef TOOLKIT_SRT_PACKET_PROFILE_HPP
 #define TOOLKIT_SRT_PACKET_PROFILE_HPP
-#include <iostream>
+#include "net/buffer.hpp"
+#include "srt_packet_common.hpp"
 namespace srt{
     /**
      * +====================+==============+=========+======================+
@@ -63,9 +64,169 @@ namespace srt{
         peer_error         = 0x0008,
         user_defined_type  = 0x7FFF,
     };
+    /**
+     * An SRT control packet has the following structure.
+     * 0                   1                   2                   3
+     * 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+     * +-+-+-+-+-+-+-+-+-+-+-+-+- SRT Header +-+-+-+-+-+-+-+-+-+-+-+-+-+
+     * |1|          Control Type      |            Subtype            |
+     * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     * |                   Type-specific Information                  |
+     * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     * |                        Timestamp                             |
+     * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     * |                   Destination Socket ID                      |
+     * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+- CIF -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     * |                                                              |
+     * +                   Control Information Field                  +
+     * |                                                              |
+     * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     *                 Figure 4: Control packet structure
+     */
+    struct control_packet_context{
+    public:
+        explicit control_packet_context(const srt_packet& pkt);
+        control_type get_control_type() const;
+        unsigned short get_sub_type() const;
+        uint32_t get_type_information() const;
+        /**
+         * 3. Packet Structure Page 7
+         *    32 bits, the timestamp of the packet, in microseconds. the value is
+         *    relative to the time the SRT connection was established.
+         */
+         uint32_t get_timestamp() const;
+        /**
+         * 3. Packet Structure Page 7
+         *    32 bits, A fixed-width field providing the SRT socket ID to which a packet
+         *    should be dispatched.the field may have the special "0" when the packet is a
+         *    connection request.
+         */
+         uint32_t get_socket_id() const;
+         const char* data() const;
+         size_t size() const;
+    private:
+        /**
+         * Control Type: 15 bits. Control Packet Type. The use of these bits
+         * is determined by the control packet type definition. See Table 1.
+         */
+        control_type type = control_type::user_defined_type;
+        /**
+         * Subtype: 16 bits. This field specifies an additional subtype for
+         * specific packets. See Table 1.
+         */
+        unsigned short sub_type = 0;
+        /**
+         * Type-specific Information: 32 bits. The use of this field depends on
+         * the particular control packet type. Handshake packets do not use
+         * this field.
+         */
+        uint32_t type_information = 0;
+        /**
+         * @See Section 3
+         */
+        uint32_t time_stamp = 0;
+        /**
+         * @See Section 3
+         */
+        uint32_t socket_id = 0;
+        const char* raw_data = nullptr;
+        size_t raw_size = 0;
+    };
 
-    struct control_packet{
-        static bool value(uint32_t val);
+    /**
+     * 0                   1                   2                   3
+     * 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+     * +-+-+-+-+-+-+-+-+-+-+-+-+- SRT Header +-+-+-+-+-+-+-+-+-+-+-+-+-+
+     * |0|                  Packet Sequence Number                     |
+     * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     * |P P|O|K K|R|            Message Number                         |
+     * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     * |                           Timestamp                           |
+     * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     * |                     Destination Socket ID                     |
+     * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     * |                                                               |
+     * +                            Data                               +
+     * |                                                               |
+     * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     *               Figure 3: Data packet structure
+     */
+    struct data_packet_context{
+    public:
+        explicit data_packet_context(const srt_packet& pkt);
+        uint32_t get_sequence() const;
+        uint32_t get_message_sequence() const;
+        uint32_t get_timestamp() const;
+        uint32_t get_socket_id() const;
+        bool is_first_packet() const;
+        bool is_single_packet() const;
+        bool is_middle_packet() const;
+        bool is_last_packet() const;
+        bool should_order() const;
+        int key_encrypt() const;
+        bool is_retransmitted() const;
+        const char* data() const;
+        size_t size() const;
+    private:
+        /**
+         * 3.1 Data packets Page 8
+         * the sequential number of the data packet.
+         */
+        uint32_t packet_sequence_number = 0;
+        /**
+         * message number: 26 bits, the sequential number of consecutive data
+         * packets that form a message (See PP field).
+         */
+        uint32_t message_number = 0;
+        /**
+         * Timestamp: 32 bits. @See section 3
+         */
+        uint32_t time_stamp = 0;
+        /**
+         * Destination Socket ID: 32 bits. @See Section 3
+         */
+        uint32_t socket_id = 0;
+        const char* raw_data = nullptr;
+        size_t length = 0;
+        /**
+         * PP: 2 bits. Packet Position Flag. This field indicates the position
+         * of the data packet in the message. The value "10b" (binary) means
+         * the first packet of the message. "00b" indicates a packet in the
+         * middle. "01b" designates the last packet. If a single data packet
+         * forms the whole message, the value is "11b".
+         */
+        char packet_position = 0;
+        /**
+         * Order Flag. Indicates whether the message should be
+         * delivered by the receiver in order (1) or not (0). Certain
+         * restrictions apply depending on the data transmission mode used
+         * (Section 4.2).
+         */
+        bool should_order_ = true;
+        /**
+         * KK: 2 bits. Key-based Encryption Flag. The flag bits indicate
+         * whether or not data is encrypted. The value "00b" (binary) means
+         * data is not encrypted. "01b" indicates that data is encrypted with
+         * an even key, and "10b" is used for odd key encryption. Refer to
+         * Section 6. The value "11b" is only used in control packets.
+         */
+        char key_encrypt_ = 0;
+        /**
+         * R: 1 bit. Retransmitted Packet Flag. This flag is clear when a
+         * packet is transmitted the first time. The flag is set to "1" when
+         * a packet is retransmitted.
+         */
+        bool is_retransmitted_ = false;
+    };
+
+    struct packet_context{
+        explicit packet_context(const srt_packet& pkt);
+
+        const data_packet_context& load_as_data() const;
+        const control_packet_context& load_as_control() const;
+    private:
+        data_packet_context* data_ptr = nullptr;
+        control_packet_context* control_ptr = nullptr;
     };
 
     struct ack{
