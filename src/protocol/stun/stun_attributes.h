f11d40a4101d6a68a8dc7668b84bc19b4106981d
sh1799 <sh1799@closeli.cn>
2022-07-29 19:14:17 +0800

    增加stun部分定义

---
+++ src/protocol/stun/stun_attributes.h
@@ -0,0 +1,51 @@
+﻿/*
+* @file_name: stun_attributes.h
+* @date: 2022/07/29
+* @author: oaho
+* Copyright @ hz oaho, All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+#ifndef TOOLKIT_STUN_ATTRIBUTES_H
+#define TOOLKIT_STUN_ATTRIBUTES_H
+namespace stun {
+    enum attributes {
+        /// Comprehension-required range
+        mapped_address = 0x0001,
+        username = 0x0006,
+        message_integrity = 0x0008,
+        error_code = 0x0009,
+        unknown_attributes = 0x000A,
+        realm = 0x0014,
+        nonce = 0x0015,
+        xor_mapped_address = 0x0020,
+        /// Comprehension-optional range (0x8000-0xFFFF)
+        software = 0x8022,
+        alternate_server = 0x8023,
+        finger_print = 0x8028,
+    };
+
+    struct attributes_helper {
+        static const char* get_attribute_name(const attributes&);
+    };
+};// namespace stun
+
+
+#endif//TOOLKIT_STUN_ATTRIBUTES_H
