08945fe5442bfaec0ab1a1434ca1ef9eafc950cd
sh1799 <sh1799@closeli.cn>
2022-08-01 13:42:57 +0800

    调整测试文件

--- test/test_srt.cpp
+++ test/test_srt.cpp
@@ -1,38 +1,43 @@
 ﻿//
 // Created by 沈昊 on 2022/4/26.
 //
-#include <protocol/srt/srt.hpp>
-#include <event_poller_pool.hpp>
-void test_address(){
-    const std::string address = "127.0.0.1";
-    auto end = asio::ip::address::from_string(address);
-    auto ad_array = end.to_v4().to_bytes();
-    const unsigned char* data = ad_array.data();
-    const uint32_t pp = *((uint32_t*)data);
-    int a = 1;
-}
-
-
-void test_connect(){
-    using namespace srt;
-    auto poller = event_poller_pool::Instance().get_poller(false);
-    asio::ip::udp::socket _socket(poller->get_executor());
-    auto core = std::make_shared<srt_core>(*poller, _socket, false);
-    auto endpoint = asio::ip::address_v4::loopback();
-    asio::ip::udp::socket::endpoint_type peer(endpoint, 5000);
-
-    core->async_connect(peer, [](const std::error_code& e){
-        Info(e.message());
-
-    });
-    event_poller_pool::Instance().wait();
-}
+//#include <protocol/srt/srt.hpp>
+//#include <event_poller_pool.hpp>
+//void test_address(){
+//    const std::string address = "127.0.0.1";
+//    auto end = asio::ip::address::from_string(address);
+//    auto ad_array = end.to_v4().to_bytes();
+//    const unsigned char* data = ad_array.data();
+//    const uint32_t pp = *((uint32_t*)data);
+//    int a = 1;
+//}
+//
+//
+//void test_connect(){
+//    using namespace srt;
+//    auto poller = event_poller_pool::Instance().get_poller(false);
+//    asio::ip::udp::socket _socket(poller->get_executor());
+//    auto core = std::make_shared<srt_core>(*poller, _socket, false);
+//    auto endpoint = asio::ip::address_v4::loopback();
+//    asio::ip::udp::socket::endpoint_type peer(endpoint, 5000);
+//
+//    core->async_connect(peer, [](const std::error_code& e){
+//        Info(e.message());
+//
+//    });
+//    event_poller_pool::Instance().wait();
+//}
+//
+//int main(){
+//
+//    logger::initialize("logs/test_logger.log", spdlog::level::trace);
+//
+//
+//    test_connect();
+//    return 0;
+//}
 
 int main(){
 
-    logger::initialize("logs/test_logger.log", spdlog::level::trace);
-
-
-    test_connect();
     return 0;
 }
--- test/test_stun.cpp
+++ test/test_stun.cpp
@@ -1,12 +1,56 @@
 ﻿//
 // Created by 沈昊 on 2022/7/29.
 //
+#include "Util/MD5.h"
 #include "protocol/stun/stun.h"
+using namespace std;
 
-int main(){
+int SASLprep(uint8_t *s) {
+    if (!s) {
+        return -1;
+    }
+    uint8_t *strin = s;
+    uint8_t *strout = s;
+    for (;;) {
+        uint8_t c = *strin;
+        if (!c) {
+            *strout = 0;
+            break;
+        }
 
+        switch (c) {
+            case 0xAD:
+                ++strin;
+                break;
+            case 0xA0:
+            case 0x20:
+                *strout = 0x20;
+                ++strout;
+                ++strin;
+                break;
+            case 0x7F:
+                return -1;
+            default:
+                if (c < 0x1F)
+                    return -1;
+                if (c >= 0x80 && c <= 0x9F)
+                    return -1;
+                *strout = c;
+                ++strout;
+                ++strin;
+        };
+    }
 
+    return 0;
+}
 
+int main() {
 
+    const std::string& buf = "pass";
+    SASLprep((uint8_t *)buf.data());
+    cout << buf << endl;
+    std::string hash = "user:realm:pass";
+    toolkit::MD5 Md5(hash);
+    cout << std::showbase << std::hex << Md5.hexdigest() << endl;
     return 0;
 }
