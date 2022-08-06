1af099be26a487d2bced3b8869d17d16ee9f5955
sh1799 <sh1799@closeli.cn>
2022-08-06 15:22:52 +0800

    增加测试文件

---
+++ test/upboundTest.cpp
@@ -0,0 +1,24 @@
+﻿//
+// Created by 沈昊 on 2022/8/6.
+//
+#include <gtest/gtest.h>
+#include <set>
+#include <vector>
+TEST(upbound, upbound) {
+
+
+
+    std::vector<int> v = {1, 2, 3, 4, 6, 7, 8};
+
+    std::set<int> sv;
+    sv.insert(v.begin(), v.end());
+    auto it = sv.upper_bound(4);
+    sv.erase(sv.begin(), it);
+
+    auto begin = sv.begin();
+    EXPECT_EQ(6, *begin);
+    ++begin;
+    EXPECT_EQ(7, *begin);
+    ++begin;
+    EXPECT_EQ(8, *begin);
+}
