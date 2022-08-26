aa328dcdd3de52fd12862ccf4632d75d6b867084
sh1799 <sh1799@closeli.cn>
2022-08-26 10:59:34 +0800

    增加hook.hpp

---
+++ 3rdlib/googletest/googletest/include/gtest/hook.hpp
@@ -0,0 +1,36 @@
+﻿//
+// Created by 沈昊 on 2022/8/26.
+//
+
+#ifndef TOOLKIT_HOOK_HPP
+#define TOOLKIT_HOOK_HPP
+#include <type_traits>
+
+// template<class T>
+// struct is_trivially_copy_constructible :
+//     std::is_trivially_constructible<T, typename std::add_lvalue_reference<
+//                                            typename
+//                                            std::add_const<T>::type>::type>
+//{};
+
+template <typename T>
+struct is_trivial_copy_
+    : public std::integral_constant<bool,
+                                    std::is_trivial<T>::value &&
+                                        std::is_copy_constructible<T>::value> {
+};
+
+template <typename A, typename B>
+struct AND : public std::conditional<A::value, B, A>::type {};
+
+template <typename T, typename... Args>
+struct is_trivial_constructable
+    : public AND<std::is_constructible<T>,
+                 std::integral_constant<bool, is_trivial_copy_<T>::value>> {};
+
+template <typename T>
+struct is_trivially_copy_constructable
+    : public is_trivial_constructable<
+          T, typename std::add_lvalue_reference<
+                 typename std::add_const<T>::type>::type> {};
+#endif  // TOOLKIT_HOOK_HPP
