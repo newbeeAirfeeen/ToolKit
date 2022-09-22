db98381a7d3b33d6b3e707c27c1b650f71008d7a
sh1799 <sh1799@closeli.cn>
2022-09-22 10:10:08 +0800

    尝试修改crash

--- src/Util/random.cpp
+++ src/Util/random.cpp
@@ -4,6 +4,7 @@
 
 
 #include "random.hpp"
+#include <ctime>
 #include <numeric>
 #include <random>
 static constexpr char CCH[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -23,9 +24,7 @@ std::string makeRandStr(int sz, bool printable) {
     return ret;
 }
 
-uint32_t rng_unsigned_integer(uint32_t range_begin, uint32_t range_end) {
-    static thread_local std::random_device ran;
-    static thread_local std::mt19937 sgen(ran());
-    std::uniform_int_distribution<> dis(range_begin, range_end);
-    return dis(sgen);
+uint32_t rng_unsigned_integer() {
+    srand(time(0));
+    return (uint32_t) rand() + 1;
 }
