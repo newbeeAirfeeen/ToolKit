6a3813daf8c8e60c529ade17dbb6d348e6e9c83d
sh1799 <sh1799@closeli.cn>
2022-09-25 18:01:42 +0800

    添加VLC依赖

---
+++ 3rdlib/vlcpp/vlc.hpp
@@ -0,0 +1,40 @@
+/*****************************************************************************
+ * vlc.hpp: Main VLC++ header.
+ *****************************************************************************
+ * Copyright © 2015 libvlcpp authors & VideoLAN
+ *
+ * Authors: Alexey Sokolov <alexey+vlc@asokolov.org>
+ *          Hugo Beauzée-Luyssen <hugo@beauzee.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifndef LIBVLC_CXX_VLC_H
+#define LIBVLC_CXX_VLC_H
+
+#include "Equalizer.hpp"
+#include "EventManager.hpp"
+#include "Instance.hpp"
+#include "Media.hpp"
+#include "MediaDiscoverer.hpp"
+#include "MediaLibrary.hpp"
+#include "MediaList.hpp"
+#include "MediaListPlayer.hpp"
+#include "MediaPlayer.hpp"
+#include "Picture.hpp"
+#include "RendererDiscoverer.hpp"
+#include "structures.hpp"
+
+#endif
